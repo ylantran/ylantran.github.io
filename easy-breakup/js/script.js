47fab900a1535d37c626c5c9425e2db912574f0a
ylan_tran <ylantran14@gmail.com>
2015-07-16 22:18:52 +0000

    qdfvgw

--- easy-breakup/js/script.js
+++ easy-breakup/js/script.js
@@ -1,7 +1,29 @@
-var sendText = function() {
+var sendText = function () {
   var phoneInput = document.getElementById('phone-number');
   var phoneNumber = phoneInput.value;
-  var nameInput = document.getElementById('name')
+  var nameInput = document.getElementById('yourname');
   var name = nameInput.value;
-  maestro.Twilio.sendSms (phoneNumber, "hey it's " + name + "." );
-  document.getElementById('message').value='';};
+  var hisOrHerNameInput = document.getElementById('hisorhername');
+  var hisOrHerName = hisOrHerNameInput.value;
+  maestro.Twilio.sendSms(phoneNumber, "Hey, it's " + name + ". I'm breaking up with you. Things just arent working..." );
+  document.getElementById('message').value='';
+};
+
+var checkresponse = function () {
+  alert('Watching for a response...')
+  maestro.Twilio.recieveSms(function(reply) {
+  var hisOrHerNameInput = document.getElementById('hisorhername');
+  var hisOrHerName = hisOrHerNameInput.value;
+  document.write("(" + reply.From + ")" + hisOrHerName + ": " + reply.Body);
+  var list = document.getElementById('demo')  
+  var firstname = document.getElementById('firstname').value
+  entry.appendChild(document.createTextNode(firstname));
+  list.appendChild(entry);
+  });
+  
+};
+
+var x = document.getElementById("mySelect");
+var option = document.createElement("option");
+option.text = "Kiwi";
+x.add(option);
